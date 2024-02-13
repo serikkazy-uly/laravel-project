b61732b8f895167ad3bb8a41386685f467292298
serikkazy-uly <damirkap89@gmail.com>
2024-02-13 23:24:42 +0600

    fix: Add route store for save img

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -4,13 +4,14 @@
 <div class="container">
     <div class="col-md-5">
         <h1>Add image</h1>
-        <form action="" method="post">
+        <form action="/store" method="post" enctype="multipart/form-data">
+            <!-- {{csrf_field()}} -->
+            @csrf
             <div class="form-control">
-                <input type="file" class="">
-
+                <input type="file" name="image">
             </div>
             <button type="submit" class="btn btn-success">Submit</button>
         </form>
     </div>
 </div>
-@endsection()  
+@endsection 
